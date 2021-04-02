7018b726f8abb63bf6640e50c5dd729091f48a4c
st3w4rd <st3w4rd.l1ttl3@gmail.com>
2021-04-02 14:33:43 +0300

    Update Lesson_1.py

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,2 +1,5 @@
-print ('Hello World')
-print ('LOL_TEST')
+time = int(input("Введите время в секундах "))
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = time - (hours * 3600 + minutes * 60)
+print( 'чаc : минута : секунда {hours} : {minutes} : {seconds}')
