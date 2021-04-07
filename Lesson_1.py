b3bdce82ab67e904360f32efb2a811b2b72eb166
gr00ve <st3w4rd.l1ttl3@gmail.com>
2021-04-07 22:18:05 +0300

    Update and rename Lesson_1.py to GeekBrains_Lesson.py
    Lesson_1 Home Work

--- Lesson_1.py
+++
@@ -1,5 +0,0 @@
-time = int(input("Введите время в секундах "))
-hours = time // 3600
-minutes = (time - hours * 3600) // 60
-seconds = time - (hours * 3600 + minutes * 60)
-print( 'чаc : минута : секунда {hours} : {minutes} : {seconds}')
